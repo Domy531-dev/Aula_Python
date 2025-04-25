f41bc47900506fdfe90dbaa2172140f66c1368d2
Vinicius <viniciussilva775.vs@gmail.com>
2025-04-25 15:45:10 -0300

    Primeiro commit

---
+++ aula09.py
@@ -0,0 +1,14 @@
+# Exercício 05
+def relogio_digital():
+    horas_limete = int(input("Digite 0 número de horas para simular: "))
+    for minuto in range(60):
+            print(f"{hora:02d}:{minuto:02d}:{segundos:02d}")
+
+relogio_digital()
+
+
+
+
+
+
+
